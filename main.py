9ed9b0f0e5ccf9d8d40b77406f5c4793dd853f2c
Sergej-bit <khalyavkin.s@yandex.ru>
2024-12-05 18:15:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+'1st program'
+print(9 ** 0.5, 3 * 5)
+'2nd program'
+print(9, 99 > 9.98 and 1000 != 1001)
+'3rd program'
+print(1234 % 1000, 234 // 10)
+print(5678 % 1000, 678 // 10)
+'4th program'
+print(13.42 == 42.13 or 13.42 != 42.13)
+print(54.39 == 37.54 or 54.39 != 37.54)
+'5 program'
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(6 == 8)
+print(123.456 * 10)
+
